19a61f50a8d6b1cfb402287ccf6fc2732c17c794
Ainur <runya579@gmail.com>
2021-08-05 22:24:18 -0400

    My User Story

--- src/test/java/com/nextBase/runners/CukesRunner.java
+++ src/test/java/com/nextBase/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/nextBase/step_definiton",
         dryRun = false,
-        tags = "@Z"
+        tags = "@a"
 )
 public class CukesRunner {
 }
